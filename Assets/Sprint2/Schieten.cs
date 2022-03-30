83e7eabc3bf2231bba63f2c1d5daab7a36aca4ee
mo <mosawer.asg@outlook.com>
2022-03-30 10:10:41 +0200

    in unity variabelen gewijzigd

--- Assets/Sprint2/Schieten.cs
+++ Assets/Sprint2/Schieten.cs
@@ -50,64 +50,60 @@ public class Schieten : MonoBehaviour
 
             Shoot();
         }
+    }
+    private void Shoot()
+    {
+        readyToShoot = false;
 
-        private void Shoot()
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        Vector3 directionwithoutSpread = targetPoint - attackPoint.position;
+
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 directionWithSpread = directionwithoutSpread + new Vector3(x, y, 0);
+
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+
+        bulletsLeft--;
+        bulletsShot++;
+
+        if (allowInvoke)
         {
-            readyToShoot = false;
-
-            Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 5f, 0));
-            RaycastHit hit;
-
-            Vector3 targetPoint;
-            if (Physics.Raycast(ray, out hit))
-                targetPoint = hit.point;
-            else
-                targetPoint = ray.GetPoint(75);
-
-            Vector3 directionwithoutSpread = targetPoint - attackPoint.position;
-
-            float x = Random.Range(-spread, spread);
-            float y = Random.Range(-spread, spread);
-
-            Vector3 directionWithSpread = directionwithoutSpread + new Vector3(x, y, 0);   
-
-            GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
-
-            currentBullet.transform.forward = directionWithSpread.normalized;
-
-            currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
-            currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
-
-
-            bulletsLeft--;
-            bulletsShot++;
-
-            if (allowInvoke)
-            {
-                Invoke("ResetShot", timeBetweenShooting);
-                allowInvoke = false;
-            }
-
-            if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
-                Invoke("Shoot", timeBetweenShots);
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
         }
 
-        private void ResetShot()
-        {
-            readyToShoot = true;
-            allowInvoke = true;
-        }
-
-        private void Reload()
-        {
-            reloading = true;
-            Invoke("ReloadFinished", reloadTime);
-        }
-
-        private void ReloadFinished()
-        {
-            bulletsLeft = magazineSize;
-            reloading = false;
-        }
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
     }
 }
